9d148d20fe2775e67e06a7854ab2947f114e222d
NicolaLino <nicola.abu.shaibeh@gmail.com>
2022-08-28 10:29:53 +0300

    small changes on methods

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 ''' Python Classes '''
 
 from typing import List, Tuple
-from datetime import date
+from datetime import datetime
 
 
 class Product():
@@ -14,36 +14,10 @@ class Product():
         self.sales_cost = sales_cost
         self.quantity = quantity
 
-    @property
-    def ID(self):
-        return self.ID
-    
-    # @ID.setter
-    # def ID(self, value):
-    #     self.ID = value
-
-    # @name.setter
-    # def name(self, value):
-    #     self.name = value
-
-    # @expiry_date.setter
-    # def expiry_date(self, value):
-    #     self.expiry_date = value
-
-    # @wholesale_cost.setter
-    # def wholesale_cost(self, value):
-    #     self.wholesale_cost = value
-
-    # @sales_cost.setter
-    # def sales_cost(self, value):
-    #     self.sales_cost = value
-
-    # @quantity.setter
-    # def quantity(self, value):
-    #     self.quantity = value
 
     def __str__(self) -> str:
-        return f"{self.ID};{self.name};{self.expiry_date};{self.wholesale_cost};{self.sales_cost};{self.quantity}"
+        # return f"Code : {self.code}, Name: {self.name}, Expiry Date: {self.expiry_date}, Wholesale Cost: {self.wholesale_cost}, Sales Cost: {self.sales_cost}, Quantity: {self.quantity}"
+        return f"{self.code};{self.name};{self.expiry_date};{self.wholesale_cost};{self.sales_cost};{self.quantity}"
 
     @staticmethod
     def is_valid_code(code_str):
@@ -53,34 +27,22 @@ class Product():
             code = int(code_str)
         except ValueError:
             return None
-        if len(code) == 4:
+        if len(str(code)) == 4:
             return code
         return None
 
 
 class Supermarket():
-    def __init__(self, code, name, address, items=None) -> None:
+    def __init__(self, code: int, name: str, address: str, items: Product = None) -> None:
         self.code = code
         self.name = name
         self.address = address
-        self.added_date = date.today()
+        self.added_date = datetime.now().strftime("%d/%m/%Y")
         if items is None:
             self.items = []
         else:
             self.items = items
 
-    # @code.setter
-    # def code(self, value):
-    #     self.code = value
-
-    # @name.setter
-    # def name(self, value):
-    #     self.name = value
-
-    # @address.setter
-    # def address(self, value):
-    #     self.address = value
-    
     def add_item(self, itm):
         if itm not in self.items:
             self.items.append(itm)
@@ -88,6 +50,3 @@ class Supermarket():
     def remove_item(self, itm):
         if itm in self.items:
             self.items.remove(itm)
-
-
-
